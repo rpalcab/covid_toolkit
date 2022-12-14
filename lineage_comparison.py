c74d6baca96e89a68aa55fe941632ce55f7bf40f
rpalcab <59095005+rpalcab@users.noreply.github.com>
2022-12-14 11:08:53 +0100

    ipynb to py, sort positions numerically

---
+++ lineage_comparison.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# Third party imports
+import argparse
+import os
+
+# Local imports
+from misc import get_markers, df_comparison
+
+def get_arguments():
+
+        parser = argparse.ArgumentParser(
+            prog='lineage_comparison.py', description='Script to ease COVID lineages comparison')
+
+        input_group = parser.add_argument_group('Input', 'Input parameters')
+        input_group.add_argument('-l', '--lin', metavar="Lineages", action='store', required=True, help='REQUIRED. Determine lineages using quotes. Two lineages are required')
+        input_group.add_argument('-t', '--thr', metavar="Threshold", action='store', type=float, default=0.9, help='Determine SNP frequency threshold. Min=0, max=1. Default=0.9')
+
+        output_group = parser.add_argument_group(
+            'Output', 'Required parameter to output results')
+        output_group.add_argument('-o', '--out', metavar="Out directory", action='store', type=str, required=True, default='.', help='REQUIRED. Output directory to extract all results')
+
+        arguments = parser.parse_args()
+
+        return arguments
+
+# Get arguments from comment line
+args = get_arguments()
+lineages = [i for i in args.lin.split(' ')]
+if len(lineages) == 2:
+    pass
+else:
+    exit('Please, introduce two COVID lineages')
+thr = args.thr
+out_path = os.path.abspath(args.out)
+
+df_list = []
+
+# Get marker SNPs of two lineages
+for i, lineage in enumerate(lineages):
+    df_list.append(get_markers(lineage, thr))
+    if df_list[i] is None:                                  # Exit if no data of a lineage
+        exit(f'Lineage {lineage} is not valid. Comparison could not be completed')
+
+# Lineages comparison
+comp_df = df_comparison(df_list, lineages)
+
+out_file = os.path.join(out_path, lineages[0] + '_' + lineages[1])
+comp_df.to_csv(out_file + '.csv', index=False)
--- misc.py
+++ misc.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import requests
+import numpy as np
 from Bio import SeqIO
 
 def get_markers(lineage, thr):
@@ -11,29 +12,27 @@ def get_markers(lineage, thr):
     r = requests.get(url)
     result = r.json()
     df = pd.json_normalize(result['data'])
-    # Removes SNPs below threshold
-    try:
+
+    try:                                                        # Removes SNPs below threshold
         df.drop(df[df['proportion'] < thr].index, inplace=True)
         df.drop(columns=['count'], inplace=True)
-    # Unless no data returned from request
-    except:
+    except:                                                     # Unless no data returned from request
         print(f'No data available for {lineage}. Will be ignored.')
         return None
 
     # Get REF, POS and ALT from df 
     df[['REF', 'POS', 'ALT']] = df['mutation'].str.extract('(\D+)(\d+)(\D+)', expand=True)
-    df.sort_values(by=['POS'], ascending=False, inplace=True)
+    df['POS'] = pd.to_numeric(df['POS'], errors='coerce')
+    df.sort_values(by=['POS'], ascending=True, inplace=True)
 
     return df
 
 def consensus(df, reference):
-    # Read Wuhan reference
-    wuhan_seq = SeqIO.parse(open(reference),'fasta')
-    # Extract fasta sequence
-    for fasta in wuhan_seq:
+    wuhan_seq = SeqIO.parse(open(reference),'fasta')            # Read Wuhan reference
+    for fasta in wuhan_seq:                                     # Extract fasta sequence
         seq = list(str(fasta.seq))
-    # Modify reference to create lineage sequence
-    for _, row in df.iterrows():
+
+    for _, row in df.iterrows():                                # Modify reference to create lineage consensus sequence
         pos = int(row['POS']) - 1
         alt = row['ALT']
         seq[pos] = alt
@@ -43,6 +42,28 @@ def consensus(df, reference):
 def write_fasta(out_file, lineage, variant_fasta):
     with open(out_file + '.fasta', 'w') as of:
         ident = '>' + lineage + '\n'
-        of.write(ident)
-        of.write(variant_fasta)
+        of.write(ident)                                         # Fasta identifier
+        of.write(variant_fasta)                                 # Fasta sequence
     return None
+
+def df_comparison(df_list, lineages):
+    df1 = df_list[0][['mutation', 'proportion']]                # Save mutations of first lineage
+    df2 = df_list[1][['mutation', 'proportion']]                # Save mutations of second lineage
+    df = df1.merge(df2, on='mutation', how='outer', indicator=True)     # Merge all SNPs in one df
+
+    df.replace({np.nan: '-',                                    # Rename values to ease understanding
+                'left_only': lineages[0], 
+                'right_only': lineages[1], 
+                'both': 'Shared'}, inplace=True)
+
+    df.rename(columns = {'proportion_x':'proportion_' + lineages[0], # Rename columns to ease understanding
+                         'proportion_y':'proportion_' + lineages[1],
+                         '_merge': 'Lineage'}, inplace = True)
+
+    df[['REF', 'POS', 'ALT']] = df['mutation'].str.extract('(\D+)(\d+)(\D+)', expand=True)
+    df['POS'] = pd.to_numeric(df['POS'], errors='coerce')
+    df.sort_values(by=['POS'], ascending=True, inplace=True)
+    col_list = ['mutation', 'REF', 'POS', 'ALT', 'proportion_BA.1', 'proportion_BA.2', 'Lineage']
+    df = df[col_list]
+
+    return df
