c093aef347f8a7400f6cebb9ca41711f719359b9
rpalcab <59095005+rpalcab@users.noreply.github.com>
2023-08-18 13:07:19 +0200

    Specify default threshold in help

--- extract_snps.py
+++ extract_snps.py
@@ -5,7 +5,7 @@ import argparse
 import os
 
 # Local imports
-from utils import get_markers, consensus, write_fasta
+from misc import get_markers, consensus, write_fasta
 
 def get_arguments():
 
@@ -14,7 +14,7 @@ def get_arguments():
 
         input_group = parser.add_argument_group('Input', 'Input parameters')
         input_group.add_argument('-l', '--lin', metavar="Lineages", action='store', required=True, help='REQUIRED. Determine lineage(s). If two or more, use quotes')
-        input_group.add_argument('-t', '--thr', metavar="Threshold", action='store', type=float, default=0.9, help='Determine threshold. Min=0, max=1')
+        input_group.add_argument('-t', '--thr', metavar="Threshold", action='store', type=float, default=0.9, help='Determine threshold. Min=0, max=1, def=0.9')
         input_group.add_argument('-r', '--ref', metavar="Reference", action='store', type=str, default=os.path.join(os.path.realpath(os.path.dirname(__file__)), 'NC_045512.2.fasta'), help='COVID reference. Default: NC_045512.2')
 
         output_group = parser.add_argument_group(
@@ -25,32 +25,26 @@ def get_arguments():
 
         return arguments
 
+# Get arguments from comment line
+args = get_arguments()
+lineages = [i for i in args.lin.split(' ')]
+thr = args.thr
+out_path = os.path.abspath(args.out)
+ref = args.ref
 
-def main():
-        # Get arguments from comment line
-        args = get_arguments()
-        lineages = [i for i in args.lin.split(' ')]
-        thr = args.thr
-        out_path = os.path.abspath(args.out)
-        ref = args.ref
+# Check if reference file exists
+if os.path.exists(ref):
+    pass
+else:
+    exit(f'Error. Reference file not found. Please check {ref}')
 
-        # Check if reference file exists
-        if os.path.exists(ref):
-            pass
-        else:
-            exit(f'Error. Reference file not found. Please check {ref}')
+# Extract SNPs of every lineage
+for lineage in lineages:
+    df = get_markers(lineage, thr)
+    if df is None:                                  # Next iteration if no SNP data
+        continue
+    variant_fasta = consensus(df, ref)
 
-        # Extract SNPs of every lineage
-        for lineage in lineages:
-            df = get_markers(lineage, thr)
-            if df is None:                                  # Next iteration if no SNP data
-                continue
-            variant_fasta = consensus(df, ref)
-
-            out_file = os.path.join(out_path, lineage)
-            df.to_csv(out_file + '.csv', index=False)       # Outuput SNP table to new_file
-            write_fasta(out_file, lineage, variant_fasta)   # Output sequence to new file
-
-               
-if __name__ == "__main__":
-        main()
+    out_file = os.path.join(out_path, lineage)
+    df.to_csv(out_file + '.csv', index=False)       # Outuput SNP table to new_file
+    write_fasta(out_file, lineage, variant_fasta)   # Output sequence to new file
